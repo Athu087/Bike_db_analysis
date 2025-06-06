57ea08328d532166ddb9ed5cc4681e11ce4f5a79
Atharva Shivaji Parle <103017457+Athu087@users.noreply.github.com>
2025-06-06 17:42:30 +0530

    Delete revenue_per_Customer.sql

--- revenue_per_Customer.sql
+++
@@ -1,5 +0,0 @@
-SELECT m.customer_id, SUM(n.quantity * n.list_price * (1 - n.discount)) AS total_revenue
-FROM orders m
-JOIN order_items n ON m.order_id = n.order_id
-GROUP BY m.customer_id
-ORDER BY total_revenue DESC;
